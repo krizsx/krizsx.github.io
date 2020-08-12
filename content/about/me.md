9a7c64a21ea0edac6c5bfa140fec3e15c80c8175
krizsx <cb.kris.bj@gmail.com>
2020-08-12 20:01:12 +0800

    add theme

---
+++ content/about/me.md
@@ -0,0 +1,11 @@
+---
+title: Me
+date: 2020-08-12T19:15:00+08:00
+lastmod: 2020-08-12T19:15:00+08:00
+---
+
+Cut out summary from your post content here.
+
+<!--more-->
+
+The remaining content of your post.
--- themes/dream/src/js/grid.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(initGrid)
-
-function initGrid() {
-  const $grid = $('.dream-grid').masonry({
-    itemSelector: '.dream-column',
-  })
-
-  $grid.imagesLoaded().progress(() => $grid.masonry('layout'))
-}
